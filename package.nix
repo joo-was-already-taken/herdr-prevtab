17f12260ad9a7fbf623aef5778d478e99dfa495f
joo-was-already-taken <s.w.wojciechowski@proton.me>
2026-09-05 11:04:15 +0200

    fix plugin version embedding on nix

--- package.nix
+++ package.nix
@@ -5,13 +5,18 @@
 let
   cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
 in
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage rec {
   pname = "herdr-prevtab";
   version = cargoToml.package.version;
   src = ./.;
 
   cargoLock.lockFile = ./Cargo.lock;
 
+  preBuild = ''
+    derivation_name="''${out##*/}"
+    full_hash="''${derivation_name%%-*}"
+    export PLUGIN_VERSION="${version}-''${full_hash}"
+  '';
   postInstall = ''
     sed -i '/^\[\[build\]\]/,/^command = /d' herdr-plugin.toml
     substituteInPlace herdr-plugin.toml \
